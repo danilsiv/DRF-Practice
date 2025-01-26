276feda5254ecc0a093c301c906a85ba99693c8f
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-26 20:13:32 +0200

    Rewrite views for Bus model with APIView

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from station.views import bus_list, bus_detail
-
+from station.views import BusListView, BusDetailView
 
 urlpatterns = [
-    path("buses/", bus_list, name="bus_list"),
-    path("buses/<int:pk>/", bus_detail, name="bus_detail"),
+    path("buses/", BusListView.as_view(), name="bus_list"),
+    path("buses/<int:pk>/", BusDetailView.as_view(), name="bus_detail"),
 ]
 
 app_name = "station"
--- station/views.py
+++ station/views.py
@@ -2,18 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from station.models import Bus
 from station.serializers import BusSerializers
 
 
-@api_view(["GET", "POST"])
-def bus_list(request):
-    if request.method == "GET":
+class BusListView(APIView):
+    def get(self, request) -> Response:
         buses = Bus.objects.all()
         serializer = BusSerializers(buses, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "POST":
+
+    def post(self, request) -> Response:
         serializer = BusSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -21,18 +22,32 @@ def bus_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def bus_detail(request, pk):
-    bus = get_object_or_404(Bus, pk=pk)
-    if request.method == "GET":
-        serializer = BusSerializers(bus)
+class BusDetailView(APIView):
+    def get_object(self, pk) -> Bus:
+        return get_object_or_404(Bus, pk=pk)
+
+    def get(self, request, pk) -> Response:
+        serializer = BusSerializers(self.get_object(pk=pk))
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "PUT":
-        serializer = BusSerializers(bus, data=request.data)
+
+    def put(self, request, pk) -> Response:
+        serializer = BusSerializers(self.get_object(pk=pk), data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        bus.delete()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk) -> Response:
+        serializer = BusSerializers(
+            self.get_object(pk=pk),
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk) -> Response:
+        self.get_object(pk=pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
