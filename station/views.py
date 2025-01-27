70c985aa7a840e2fbc4520b27adf6f0876c61af8
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-27 11:52:56 +0200

    Rewrite and optimize views implementation with generics

--- station/views.py
+++ station/views.py
@@ -1,42 +1,14 @@
-from rest_framework.response import Response
-from rest_framework import generics, mixins
+from rest_framework import generics
 
 from station.models import Bus
 from station.serializers import BusSerializers
 
 
-class BusListView(
-    generics.GenericAPIView,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin
-):
+class BusListView(generics.ListCreateAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializers
 
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs) -> Response:
-        return self.create(request, *args, **kwargs)
-
-
-class BusDetailView(
-    generics.GenericAPIView,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin
-):
+class BusDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializers
-
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs) -> Response:
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs) -> Response:
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs) -> Response:
-        return self.destroy(request, *args, **kwargs)
