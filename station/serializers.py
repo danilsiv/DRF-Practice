2a40dce541a8cbb61a82afa3a805924d6d1891ac
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-27 12:40:40 +0200

    Rewrite BusSerializer using ModelSerializer

--- station/serializers.py
+++ station/serializers.py
@@ -2,17 +2,8 @@ from rest_framework import serializers
 from station.models import Bus
 
 
-class BusSerializers(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    info = serializers.CharField(max_length=255, required=False)
-    num_seats = serializers.IntegerField(required=True)
+class BusSerializers(serializers.ModelSerializer):
 
-    def create(self, validated_data) -> Bus:
-        return Bus.objects.create(**validated_data)
-
-    def update(self, instance, validated_data) -> Bus:
-        instance.info = validated_data.get("info", instance.info)
-        instance.num_seats = validated_data.get("num_seats", instance.num_seats)
-        instance.save()
-
-        return instance
+    class Meta:
+        model = Bus
+        fields = ("id", "info", "num_seats")
