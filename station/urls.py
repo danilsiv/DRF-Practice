c80e147777f966817ba51e5e1da25cee428f2dce
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-27 12:08:35 +0200

    Rewrite views with viewssets and change mapping logic in urls

--- station/urls.py
+++ station/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from station.views import BusListView, BusDetailView
+from station.views import BusListView
+
+bus_list = BusListView.as_view(actions={"get": "list", "post": "create"})
+bus_detail = BusListView.as_view(
+    actions = {
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
 
 urlpatterns = [
-    path("buses/", BusListView.as_view(), name="bus_list"),
-    path("buses/<int:pk>/", BusDetailView.as_view(), name="bus_detail"),
+    path("buses/", bus_list, name="bus_list"),
+    path("buses/<int:pk>/", bus_detail, name="bus_detail"),
 ]
 
 app_name = "station"
--- station/views.py
+++ station/views.py
@@ -1,14 +1,16 @@
-from rest_framework import generics
+from rest_framework import viewsets, mixins
 
 from station.models import Bus
 from station.serializers import BusSerializers
 
 
-class BusListView(generics.ListCreateAPIView):
-    queryset = Bus.objects.all()
-    serializer_class = BusSerializers
-
-
-class BusDetailView(generics.RetrieveUpdateDestroyAPIView):
+class BusListView(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
     queryset = Bus.objects.all()
     serializer_class = BusSerializers
