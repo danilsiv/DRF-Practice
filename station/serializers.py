2fa7a52bc9dd7602c3688371c9aeb8b037cd19b0
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-29 20:23:18 +0200

    Create serializers for Trip model

--- station/serializers.py
+++ station/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from station.models import Bus
+from station.models import Bus, Trip
 
 
 class BusSerializers(serializers.ModelSerializer):
@@ -7,3 +7,14 @@ class BusSerializers(serializers.ModelSerializer):
     class Meta:
         model = Bus
         fields = ("id", "info", "num_seats", "is_small")
+
+
+class TripSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Trip
+        fields = ("id", "source", "destination", "departure", "bus")
+
+
+class TripListSerializer(TripSerializer):
+    bus = BusSerializers()
