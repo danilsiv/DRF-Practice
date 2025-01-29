9e4e56ca15e182701249e9af9c0dba92e0bf3405
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-01-29 20:23:56 +0200

    Create TripViewSet with url mapping

--- station/urls.py
+++ station/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from station.views import BusViewSet
 from rest_framework import routers
+
+from station.views import (
+    BusViewSet,
+    TripViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register("buses", BusViewSet)
+router.register("trips", TripViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- station/views.py
+++ station/views.py
@@ -1,9 +1,28 @@
 from rest_framework import viewsets
 
-from station.models import Bus
-from station.serializers import BusSerializers
+from station.models import Bus, Trip
+from station.serializers import BusSerializers, TripSerializer, TripListSerializer
 
 
 class BusViewSet(viewsets.ModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusSerializers
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+    def get_serializer_class(self) -> object:
+        serializer = self.serializer_class
+        if self.action == "list":
+            serializer = TripListSerializer
+
+        return serializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            queryset = queryset.select_related()
+
+        return queryset
