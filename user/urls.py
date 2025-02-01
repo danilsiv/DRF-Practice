ff80e7848a82d8618c3df917eeb37f493c4022da
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-01 16:53:38 +0200

    Add LoginUserView with url mapping and new migration

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView
 
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="get_token"),
 ]
 
 app_name = "user"
